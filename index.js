405220cb8d619a4ac19a673ddf24390e9a13c9e0
MNdagu <ndagumark@gmail.com>
2024-06-29 17:10:12 +0300

    Final commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function gradeGenerator(){
+    let mark = prompt('Enter mark:');
+
+    if(isNaN(mark) || mark > 100){
+        alert('Please enter a valid number for your mark.')
+        return;
+    }
+
+    if (mark >= 80){
+        document.getElementById("grade").innerHTML= 'You got an A!';
+    }else if(mark >= 60){
+        document.getElementById("grade").innerHTML= 'You got an B!';;
+    }else if(mark >= 50){
+        document.getElementById("grade").innerHTML= 'You got an C!';;
+    }else if(mark >= 40){
+        document.getElementById("grade").innerHTML= 'You got an D!';;
+    }else {
+        document.getElementById("grade").innerHTML= 'You got an E!';;
+    }
+    return;
+
+}
